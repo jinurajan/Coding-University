8e96418250faca8c4d1a48c7d9e24862def229fa
JINU RAJAN <jinu.p.r@gmail.com>
2021-12-21 23:30:18 +0000

    dp problems educative.io added print selected elements

--- dynamic_programming/knapsack.py
+++ dynamic_programming/knapsack.py
@@ -109,6 +109,40 @@ def solve_knapsack_bottomup(profits, weights, capacity):
             dp[i][c] = max(p1, p2)
     return dp[n-1][capacity]
 
+def solve_knapsack_bottomup_with_printing(profits, weights, capacity):
+    n = len(profits)
+    if capacity <= 0 or n == 0 or len(weights) != n:
+        return 0
+    dp =[[0 for x in range(capacity+1)] for y in range(n)]
+    for c in range(0, capacity+1):
+        if weights[0] <= c:
+            dp[0][c] = profits[0]
+    for i in range(1, n):
+        for c in range(1, capacity+1):
+            p1, p2 = 0, 0
+            if weights[i] <= c:
+                p1 = profits[i] + dp[i-1][c-weights[i]]
+            p2 = dp[i-1][c]
+            dp[i][c] = max(p1, p2)
+    print_selected_elements(dp, weights, profits, capacity)
+    return dp[n-1][capacity]
+
+
+
+def print_selected_elements(dp, weights, profits, capacity):
+    print("Selected weights are: ", end='')
+    n = len(weights)
+    totalProfit = dp[n-1][capacity]
+    for i in range(n-1, 0, -1):
+        if totalProfit != dp[i - 1][capacity]:
+            print(str(weights[i]) + " ", end='')
+            capacity -= weights[i]
+            totalProfit -= profits[i]
+
+    if totalProfit != 0:
+        print(str(weights[0]) + " ", end='')
+    print()
+
 
 
 print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
@@ -123,3 +157,6 @@ print(solve_knapsack_topdown_1([1, 6, 10, 16], [1, 2, 3, 5], 6))
 
 print(solve_knapsack_bottomup([1, 6, 10, 16], [1, 2, 3, 5], 7))
 print(solve_knapsack_bottomup([1, 6, 10, 16], [1, 2, 3, 5], 6))
+
+print(solve_knapsack_bottomup_with_printing([1, 6, 10, 16], [1, 2, 3, 5], 7))
+print(solve_knapsack_bottomup_with_printing([1, 6, 10, 16], [1, 2, 3, 5], 6))
